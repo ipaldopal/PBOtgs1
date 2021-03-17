ebee493f30604618279ba2cbdf91ebc9036e4295
ipaldopal <hamnaya080802@gmail.com>
2021-03-17 14:47:03 +0700

    initial commit

---
+++ src/GallonPBO1/tgsGallon1.java
@@ -0,0 +1,21 @@
+package GallonPBO1;
+
+import java.util.Scanner;
+
+public class tgsGallon1 {
+
+    public static void main(String[] args) {
+        Scanner ambil = new Scanner(System.in);
+
+        double Gallons;
+        double rms;
+
+        System.out.println("==== Konversi Satuan Liter Galon ===\t");
+        System.out.print("\n\t   Masukkan Jumlah Galon : ");
+
+        Gallons=ambil.nextDouble();
+        rms=Gallons*3.785f;
+
+        System.out.printf("\t\t%.0f Galon = %.3f Liter \n\n",Gallons,rms);
+    }
+}
